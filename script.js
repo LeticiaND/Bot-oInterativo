523302c932ca936487d6569540d43dd5cdd5894b
Leticia <leticiandejesus@icloud.com>
2023-02-28 18:07:15 -0300

    salvando arquivo

--- script.js
+++ script.js
@@ -16,17 +16,29 @@ function checarOsInputs(){
     }else{
         setarSucessoPara(username)
     }
+    if(passwordValue === ""){
+        setarErroPara (password, "A senha é obrigatória")
+    }else if(passwordValue.length < 7){
+        setarErroPara (password, "A senha precisa ter no mínimo 7 caracteres")
+    }else{
+        setarSucessoPara(password)
+    }
 }
 
-function setarErroPara(input, mensagem){
-    const formControl = input.parentElement
-    const small = formControl.querySelector("small")
+function setarErroPara(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector("small");
+  
+    // Adiciona a mensagem de erro
+    small.innerText = message;
+  
+    // Adiciona a classe de erro
+    formControl.className = "form-control error";
+  }
 
-    formControl.className = "form-control error"
-}
-
-function setarSucessoPara(input){
-    const formControl = input.parentElement
-
-    formControl.className = "form-control error"
-}
+  function setarSucessoPara(input) {
+    const formControl = input.parentElement;
+  
+    // Adicionar a classe de sucesso
+    formControl.className = "form-control success";
+  }
